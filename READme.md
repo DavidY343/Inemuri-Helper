1d2a590cf8c73645e78288763c6285eeda0e5ff3
david <tipiraenkewa1@gmail.com>
2024-02-21 09:40:50 +0100

    acabado

---
+++ READme.md
@@ -0,0 +1,41 @@
+# README - Proyecto Inemuri Helper
+
+## Descripción del Proyecto
+
+El proyecto "Inemuri Helper" es una aplicación web que proporciona ayuda a los usuarios que desean tomar siestas o dormir durante sus viajes o trayectos, alertándolos cuando están cerca de su destino deseado.
+
+La aplicación utiliza la tecnología de geolocalización para rastrear la posición del usuario y determinar su proximidad al destino establecido. Cuando el usuario se acerca a su destino, la aplicación emite una alarma audible y vibra para alertar al usuario de que está cerca de su parada. Además, proporciona una interfaz interactiva para seleccionar diferentes sonidos de alarma.
+
+## Funcionalidades requeridas
+
+1. Definir el lugar de destino en el mapa (Para ello con hacer click sobre el mapa un marcador de destino aparece.).
+2. Controlar si el usuario está en proximidad de su destino.
+3. Vibrar si el usuario está cerca de su destino.
+
+## Funcionalidades adicionales
+
+1. Sonido de alarma cuando el usuario esta cerca de su destino.
+2. Boton (rojo) que al hacer click centra la pantalla en el usuario.
+3. Boton (azul) que abre un menu desplegable y eliges el sonido entre cuatro opciones.
+4. Barra de busqueda que permite buscar el destino.
+
+## APIs usadas
+
+- **Leaflet**: Biblioteca de mapas interactivos de código abierto para la creación de mapas móviles.
+- **Leaflet Control Geocoder**: Plugin de Leaflet para proporcionar una función de búsqueda y geocodificación de direcciones.
+- **SweetAlert2**: Librería de JavaScript para interfaz cuando suena la alarma
+
+## Cómo Usar
+
+1. Abre la aplicación en un navegador web compatible con geolocalización.
+2. Selecciona un sonido de alarma de la lista desplegable disponible.
+3. Haz clic en el mapa para establecer tu destino.
+4. La aplicación te alertará cuando te acerques a tu destino con la alarma seleccionada y vibración.
+5. Si deseas desactivar la alarma, confirma la acción en el cuadro de diálogo que aparece.
+
+## Decisiones de diseño
+
+1. 1 Kilometro o menos es lo que se necesita para activar la alarma.
+2. Al abrir la pagina web, te centra en el usuario.
+3. Solo puedo exister un marcador de destino a la vez.
+3. Cuando la alarma se activa aparece un evento para desactivarla y tambien borra el marcador de destino.
--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //Creating icons for the user and the destination
 var UserIcon = L.icon({
-	iconUrl: 'userMarker.png',
+	iconUrl: 'media/userMarker.png',
 	iconSize: [50, 50],
 	iconAnchor: [25, 50],
 	popupAnchor: [0, -50]
 });
 var destinationIcon = L.icon({
-	iconUrl: 'destinationMarker.png',
+	iconUrl: 'media/destinationMarker.png',
 	iconSize: [50, 50],
 	iconAnchor: [25, 50],
 	popupAnchor: [0, -50] 
@@ -16,10 +16,10 @@ var destinationIcon = L.icon({
 let lat = 40.3262; //Latitud y longitud por defecto Leganes
 let lon = -3.7589;
 let destinoMarker = null; // por defecto no existe un destino
-let alarmaaa = new Audio('alarmaaa.mp3');
-let battleship_alarm = new Audio('battleship-alarm.wav');
-let citysiren_alarm = new Audio('citysiren-alarm.wav');
-let freaks_alarm = new Audio('freaks-alarm.mp4');
+let alarmaaa = new Audio('media/alarmaaa.mp3');
+let battleship_alarm = new Audio('media/battleship-alarm.wav');
+let citysiren_alarm = new Audio('media/citysiren-alarm.wav');
+let freaks_alarm = new Audio('media/freaks-alarm.mp4');
 let audio = alarmaaa; // Establece por defecto alarmaaa
 let alarmSounds = {
     'alarmaaa': alarmaaa,
@@ -28,67 +28,90 @@ let alarmSounds = {
     'freaks_alarm': freaks_alarm
 };
 
+// Menu desplegable para seleccionar el sonido de la alarma
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdownBtn = document.querySelector('.dropbutton');
+    var dropdownContent = document.querySelector('.dropdown-content');
+
+    dropdownBtn.addEventListener('click', function() {
+        dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+    });
+
+    var dropdownOptions = document.querySelectorAll('.dropdown-content a');
+    dropdownOptions.forEach(function(option) {
+        option.addEventListener('click', function() {
+            dropdownContent.style.display = 'none';
+        });
+    });
+
+	document.querySelectorAll('.dropdown-content a').forEach(function(option) {
+		option.addEventListener('click', function() {
+			audio = alarmSounds[this.textContent];
+		});
+	});
+});
 
 if (navigator.geolocation) {
 
-	document.addEventListener('DOMContentLoaded', function() {
-		document.querySelectorAll('.dropdown-content a').forEach(function(option) {
-			option.addEventListener('click', function() {
-				audio = alarmSounds[this.textContent];
-			});
-		});
-	});
-
+	// Obtiene la ubicación del usuario
 	navigator.geolocation.getCurrentPosition(function(position) {
 		lat = position.coords.latitude;
 		lon = position.coords.longitude;
 	});
 
+	// Inicializamos mapa
 	const mymap = L.map('map').setView([lat, lon], 17);
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
 	maxZoom: 18
 	}).addTo(mymap);
 
+	// Centra el mapa al inicio
+	function centrarAlEmpezar() {
+		mymap.setView([lat, lon], 17);
+	}
+	setTimeout(centrarAlEmpezar, 1000);
+
+	// Añade un unico marcador en el mapa al hacer clic
 	mymap.on('click', function(e) {
 		if (destinoMarker) {
 			mymap.removeLayer(destinoMarker);
 		}
 		destinoMarker = L.marker(e.latlng, { icon: destinationIcon }).addTo(mymap);
 	});
-  
+
+	// Añade un marcador en el mapa para el usuario, que se actualiza en tiempo real
 	let userMarker = L.marker([lat, lon], { icon: UserIcon }).addTo(mymap);
-  
 	navigator.geolocation.watchPosition(function(position) {
 		let lat = position.coords.latitude;
 		let lon = position.coords.longitude;
 		let newLatLng = new L.LatLng(lat, lon);
 		userMarker.setLatLng(newLatLng);
+		// Funcionalidad de la alarma
 		if (destinoMarker)
 		{
 			let distanceToDestination = mymap.distance(newLatLng, destinoMarker.getLatLng());
 			let volume = 1 - (distanceToDestination / 500);
-			// Si el usuario está a menos de 500 metros del destino, vibra y suena una alarma
-			if (distanceToDestination < 500)
+			// Si el usuario está a menos de 1 km metros del destino, vibra y suena una alarma
+			if (distanceToDestination < 1000)
 			{
-				navigator.vibrate(1000); // Vibra durante 1000 milisegundos
+				navigator.vibrate(1000);
 				audio.volume = volume;
 				audio.play();
 				// Muestra un cuadro de diálogo al usuario
 				Swal.fire({
 					title: 'Has llegado a tu destino',
 					text: '¿Quieres desactivar la alarma?',
-					icon: 'warning',
 					showCancelButton: true,
-					confirmButtonText: 'Sí',
-					cancelButtonText: 'No',
-					backdrop: 'rgba(0,0,0,0)',
+					confirmButtonColor: '#3085d6',
+					cancelButtonColor: '#d33',
+					confirmButtonText: 'Apagar',
 					allowOutsideClick: false
 				}).then((result) =>
 				{
 					if (result.isConfirmed)
 					{
-						// Si el usuario hace clic en "Sí", detén la alarma
+						// Si el usuario hace clic en "Sí", detén la alarma y desaparece el marcador del destino
 						audio.pause();
 						audio.currentTime = 0;
 						navigator.vibrate(0);
@@ -99,19 +122,19 @@ if (navigator.geolocation) {
 			}
 			else
 			{
-				audio.pause(); // Pausa el sonido
-				audio.currentTime = 0; // Opcional: rebobina el audio para la próxima vez que se reproduzca
-				navigator.vibrate(0); // Detiene la vibración
+				audio.pause();
+				audio.currentTime = 0;
+				navigator.vibrate(0);
 			}
 		}
 	});
-  
+
+	// Añade un control de búsqueda de direcciones
 	let geocoder = L.Control.geocoder({
 		collapsed: false,
 		placeholder: 'Buscar dirección...',
 		defaultMarkGeocode: false
 	}).addTo(mymap);
-
 	geocoder.on('markgeocode', function(e) {
 	if (destinoMarker)
 	{
@@ -119,24 +142,19 @@ if (navigator.geolocation) {
 	}
 		let latlng = e.geocode.center;
 		destinoMarker = L.marker(latlng, { icon: destinationIcon }).addTo(mymap);
-
-		// Centrar el mapa en las coordenadas del lugar marcado
 		mymap.setView(latlng);
 	});
 
+	// Botón para centrar el mapa en la ubicación del usuario
 	document.getElementById('centerButton').addEventListener('click', function() {
-		console.log("Center button clicked");
 		navigator.geolocation.getCurrentPosition(function(position) {
 			let lat = position.coords.latitude;
 			let lon = position.coords.longitude;
-			console.log("Centering map at", lat, lon);
 			mymap.setView([lat, lon], 17);
 		});
-
 	});
 }
-else
+else // Si el navegador no soporta geolocalización
 {
 	alert("Could not get your location");
-	console.log("Could not get your location");
 }
